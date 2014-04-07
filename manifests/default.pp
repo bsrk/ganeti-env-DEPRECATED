24d2f48677447ddc24f3fa937e50762e858beadd
BSRK Aditya <bsrk@google.com>
2014-04-07 16:46:11 +0530

    add hosts for instances and cluster

--- manifests/default.pp
+++ manifests/default.pp
@@ -11,6 +11,26 @@ host {
     ensure => present,
     host_aliases => 'node3',
     ip => '33.33.33.13';
+  'cluster.example.org':
+    ensure => present,
+    host_aliases => 'cluster',
+    ip => '33.33.33.21';
+  'instance1.example.org':
+    ensure => present,
+    host_aliases => 'instance1',
+    ip => '33.33.33.31';
+  'instance2.example.org':
+    ensure => present,
+    host_aliases => 'instance2',
+    ip => '33.33.33.32';
+  'instance3.example.org':
+    ensure => present,
+    host_aliases => 'instance3',
+    ip => '33.33.33.33';
+  'instance4.example.org':
+    ensure => present,
+    host_aliases => 'instance4',
+    ip => '33.33.33.34';
 }
 package {
   'ubuntu-virt-server':
