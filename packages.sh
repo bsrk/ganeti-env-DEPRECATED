968cbb5563421b66c948000a2fe3b3e0a26d68ba
BSRK Aditya <bsrk@google.com>
2014-04-07 16:36:37 +0530

    add packages docutils sphinx

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-easy_install pyOpenSSL affinity pyyaml
+easy_install pyOpenSSL affinity pyyaml docutils sphinx
 
 cabal update
 cabal install --global lifted-base lens base64-bytestring snap-server hinotify hslogger network attoparsec json parallel utf8-string curl Crypto regex-pcre vector
